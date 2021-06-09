fe679be26d8bf8816d38de3db245bf8c69eafa05
tanifumiya <fumiyadeveloper@gmail.com>
2021-06-10 02:27:02 +0900

    Preview表示まで行えるように修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,125 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TakePictureScreen(camera: firstCamera),
+    ),
+  );
+}
+
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const TakePictureScreen({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.max,
+    );
+    _controller.initialize().then<void>((_) {
+      if (!mounted) {
+        return;
+      } else {
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Take a Picture'),
+      ),
+      body: _buildBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          takePicture();
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    if (_controller.value.isInitialized) {
+      return CameraPreview(_controller);
+    } else {
+      return CircularProgressIndicator();
+    }
+  }
+
+  Future<void> takePicture() async {
+    try {
+      final image = await _controller.takePicture();
+
+      await Navigator.of(context).push<void>(
+        MaterialPageRoute(
+          builder: (BuildContext context) => DisplayPictureScree(
+            imagePath: image.path,
+          ),
+        ),
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+}
+
+class DisplayPictureScree extends StatelessWidget {
+  final String imagePath;
+
+  DisplayPictureScree({
+    Key? key,
+    required this.imagePath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Display Picture'),
+      ),
+      body: Image.file(
+        File(imagePath),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
